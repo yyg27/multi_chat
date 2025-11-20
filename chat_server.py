0d9315e1c63931548a320ea1d5629a70c51ed2d9
yyg27 <yyg27@outlook.com>
2025-11-20 22:52:49 +0300

    -added spam protection -added cooldown time for spammers -made the code for modular for incoming server options -added stats

--- chat_server.py
+++ chat_server.py
@@ -2,20 +2,208 @@ import socket
 import threading
 import logging
 import random
+import time
 from datetime import datetime
+
 
 #logging config
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("logs/chat_server.log"), #save chat server logs
+        logging.FileHandler("chat_server.log"), #save chat server logs
         logging.StreamHandler() #write to console
     ]
 );
 
+host = "0.0.0.0";
+tcp_port = 56458;
+
+#for storing clients address and usernames
+clients = [];
+usernames = [];
+
+server_stats = {
+    "total_messages":0,
+    "total_pms":0,
+    "active_users":0
+};
+
+#to keep last message times for clients
+last_message_time = {};
+muted_users = {}
+
+def is_spam(client):
+    current_time = time.time();
+    spam_timer = 1
+    cooldown = 10
+
+    #check if user is in already cooldown
+    if client in muted_users:
+        cooldown_time = muted_users[client];
+        if current_time < cooldown_time:
+            cooldown = int(cooldown_time - current_time); ##new cooldown value
+            return True,cooldown;
+        else:
+            del muted_users[client];
+
+    #check if is spam
+    if client in last_message_time:
+        last_time = last_message_time[client]; 
+
+        if current_time - last_time < spam_timer:
+            muted_users[client] = current_time + cooldown
+            return True,cooldown #spam
+
+    last_message_time[client] = current_time;
+    return False,0;
+
+
+def show_stats():
+    server_stats["active_users"] = len(clients);
+
+
+##function to receive connections
+def receive(server):
+    while True:
+        try:
+            client, address = server.accept();
+            logging.info(f"User connected from {address}");
+                
+            client.send("USERNAME".encode("utf-8"));
+            username = client.recv(1024).decode("utf-8", errors="ignore").strip();
+                
+            ##username rules
+            if username.startswith('*'):
+                client.send("[### CHAT SERVER ###] - Invalid username! Username cannot start with *".encode('utf-8'));
+                client.close();
+                continue
+
+            if username in usernames:
+                existing_username = username
+                while username in usernames:
+                    suffix = random.randint(0,999);
+                    username = f"{existing_username}{suffix}";
+                client.send(f"[### CHAT SERVER ###] - Username is already in use.. Your username has been changed to {username}".encode('utf-8'));    
+                
+            if not username:
+                username = f"User_{address[1]}";
+                
+            #append to server list
+            clients.append(client);
+            usernames.append(username);
+                
+            print(f"Your username is : {username}")
+            broadcast(f"[### CHAT SERVER ###] - {username} has joined the chat");
+            client.send("[### CHAT SERVER ###] - Connected to the server".encode("utf-8"));
+            logging.info(f"{username} has joined the chat from {address}");
+                
+            #threading
+            thread = threading.Thread(target=handle, args=(client,), daemon=True);
+            ##daemon=True prevents crashes
+            thread.start();
+        except Exception as e:
+            logging.error(f"Error accepting connection: {e}");
+            break
+    
+##function to handle client messages
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024).decode("utf-8", errors="ignore");
+            if not message:
+                #if client disconnected
+                raise Exception("Client disconnected");
+            
+            #check spam
+            spam,cooldown = is_spam();
+
+            if spam:
+                client.send(f"SPAM DETECTED ! You are muted for {cooldown} seconds...".encode('utf-8'));
+                continue;
+    
+            #find username of the sender
+            index = clients.index(client);
+            username = usernames[index];
+                
+            #find out if it is a broadcast or private message
+            if message.startswith("/pm"):
+                send_pm(client,username,message);
+            else:
+                if is_spam():
+                    client.send("SPAM DETECTED ")
+                #add username to message
+                message_to_broadcast = f"{username}: {message}";
+                broadcast(message_to_broadcast);
+
+        except:
+            #remove client
+            if client in clients:
+                index = clients.index(client);
+                clients.remove(client);
+                client.close();
+                username = usernames[index];
+                broadcast(f"{username} has left the chat");
+                usernames.remove(username);
+                logging.info(f"{username} disconnected");
+            break
+    
+
+##function to broadcast messages to all clients
+def broadcast(message):
+    #add timestamps
+    timestamp = datetime.now().strftime('%H:%M:%S');
+        
+    #turn message to utf-8 format
+    if isinstance(message, bytes):
+        message = message.decode('utf-8');
+            
+    formatted_message = f"[{timestamp}] {message}";
+        
+    logging.info(f"[BROADCAST]: {formatted_message}");
+
+    for client in clients:
+        try:
+          client.send(formatted_message.encode('utf-8'));
+        except Exception as e:
+            logging.error(f"Error broadcasting to client: {e}");
+
+
+##function to send private messages
+def send_pm(sender_client,sender_username,message):
+    try:
+        ##message format is /pm username hello world blablabla
+        components = message.split(" ",2);
+
+        if len(components) < 3:
+            sender_client.send("[### CHAT SERVER ###] - Invalid /pm command use this format: /pm <username> <message>".encode('utf-8'));
+            return
+
+        target_name = components[1];
+        private_message = components[2];
+
+        if target_name not in usernames:
+            sender_client.send(f"[### CHAT SERVER ###] - User {target_name} is not found".encode('utf-8'));
+
+        timestamp = datetime.now().strftime('%H:%M:%S');
+
+        target_index = usernames.index(target_name);
+        target_client = clients[target_index];
+
+        #send pm
+        target_client.send(f"[{timestamp}] [*PRIVATE*] {sender_username}: {private_message}".encode('utf-8'));
+        #to notify the sender
+        sender_client.send(f"[{timestamp}] [PM sended to {target_name}]: {private_message}".encode('utf-8'))
+        #logging
+        logging.info(f"[**PRIVATE MESSAGE**] {sender_username} to {target_name} : {private_message}");
+
+    except Exception as e:
+        logging.error(f"PM Error: {e}");
+        sender_client.send("[### CHAT SERVER ###] - Error sending private message.".encode('utf-8'));
+
+
 ##TCP chat server##
-def chat_server(host="0.0.0.0", port=56458):
+def tcp_chat_server():
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
@@ -31,148 +219,22 @@ def chat_server(host="0.0.0.0", port=56458):
         logging.error(f"[### CHAT SERVER ###] - Failed to start server: {e}");
         return
     
-    #for storing clients address and usernames
-    clients = [];
-    usernames = [];
-    
-    ##function to broadcast messages to all clients
-    def broadcast(message):
-        #add timestamps
-        timestamp = datetime.now().strftime('%H:%M:%S');
-        
-        #turn message to utf-8 format
-        if isinstance(message, bytes):
-            message = message.decode('utf-8');
-            
-        formatted_message = f"[{timestamp}] {message}";
-        
-        logging.info(f"[BROADCAST]: {formatted_message}");
 
-        for client in clients:
-            try:
-              client.send(formatted_message.encode('utf-8'));
-            except Exception as e:
-                logging.error(f"Error broadcasting to client: {e}");
     
-    ##function to send private messages
-    def send_pm(sender_client,sender_username,message):
+try:
+    receive();
+except KeyboardInterrupt:
+    logging.info("Server shutting down...");
+    for client in clients:
         try:
-            ##mesage format is /pm yyg hello world etc
-            components = message.split(" ",2);
-
-            if len(components) < 3:
-                sender_client.send("[### CHAT SERVER ###] - Invalid /pm command use this format: /pm <username> <message>".encode('utf-8'));
-                return
-
-            target_name = components[1];
-            private_message = components[2];
-
-            if target_name not in usernames:
-                sender_client.send(f"[### CHAT SERVER ###] - User {target_name} is not found".encode('utf-8'));
-
-            timestamp = datetime.now().strftime('%H:%M:%S');
-
-            target_index = usernames.index(target_name);
-            target_client = clients[target_index];
-
-            #send pm
-            target_client.send(f"[{timestamp}] [*PRIVATE*] {sender_username}: {private_message}".encode('utf-8'));
-            #to notify the sender
-            sender_client.send(f"[{timestamp}] [PM sended to {target_name}]: {private_message}".encode('utf-8'))
-            #logging
-            logging.info(f"[**PRIVATE MESSAGE**] {sender_username} to {target_name} : {private_message}");
-
-        except Exception as e:
-            logging.error(f"PM Error: {e}")
-            sender_client.send("[### CHAT SERVER ###] - Error sending private message.".encode('utf-8'))
-
-
-    ##function to handle client messages
-    def handle(client):
-        while True:
-            try:
-                message = client.recv(1024).decode("utf-8", errors="ignore");
-                if not message:
-                    #if client disconnected
-                    raise Exception("Client disconnected");
-                
-                #find username of the sender
-                index = clients.index(client);
-                username = usernames[index];
-                
-                #find out if it is a broadcast or private message
-                if message.startswith("/pm"):
-                    send_pm(client,username,message);
-                else:
-                #add username to message
-                    message_to_broadcast = f"{username}: {message}";
-                    broadcast(message_to_broadcast);
-
-            except:
-                #remove client
-                if client in clients:
-                    index = clients.index(client);
-                    clients.remove(client);
-                    client.close();
-                    username = usernames[index];
-                    broadcast(f"{username} has left the chat");
-                    usernames.remove(username);
-                    logging.info(f"{username} disconnected");
-                break
-    
-    ##function to receive connections
-    def receive():
-        while True:
-            try:
-                client, address = server.accept();
-                logging.info(f"User connected from {address}");
-                
-                client.send("USERNAME".encode("utf-8"));
-                username = client.recv(1024).decode("utf-8", errors="ignore").strip();
-                
-                ##username rules
-                if username.startswith('*'):
-                    client.send("[### CHAT SERVER ###] - Invalid username! Username cannot start with *".encode('utf-8'));
-                    client.close();
-                    continue
-
-                if username in usernames:
-                    existing_username = username
-                    while username in usernames:
-                        suffix = random.randint(0,999);
-                        username = f"{existing_username}{suffix}";
-                    client.send("[### CHAT SERVER ###] - Username is already in use.. Your username has been changed to {username}".encode('utf-8'));    
-                
-                if not username:
-                    username = f"User_{address[1]}";
-                
-                #append to server list
-                clients.append(client);
-                usernames.append(username);
-                
-                print(f"Your username is : {username}")
-                broadcast(f"[### CHAT SERVER ###] - {username} has joined the chat");
-                client.send("[### CHAT SERVER ###] - Connected to the server".encode("utf-8"));
-                logging.info(f"{username} has joined the chat from {address}");
-                
-                #threading
-                thread = threading.Thread(target=handle, args=(client,), daemon=True);
-                ##daemon=True prevents crashes
-                thread.start();
-            except Exception as e:
-                logging.error(f"Error accepting connection: {e}");
-                break
-    
-    try:
-        receive();
-    except KeyboardInterrupt:
-        logging.info("Server shutting down...");
-        for client in clients:
-            try:
-                client.close();
-            except:
-                pass
-        server.close();
+            client.close();
+        except:
+            pass
+    server.close();
 
 if __name__ == "__main__":
-    chat_server();
+    tcp_chat_server();
+
+
+
+    
